4acd640814ee46dad7fd69b635f2bb2761db58df
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-16 02:47:20 +0300

    line arrng

---
+++ line.py
@@ -0,0 +1,17 @@
+
+a = int(input("Enter the number of students"))
+c= 10-a
+b=a-10
+for a in range(1 or 20):
+    if  a==10:
+        print("come eat")
+    elif a>10:
+        # b=a-10
+        print(f"the {b}wait for next round")
+    elif a<10:
+        print(f"we need {c} more students" ) 
+    else:
+        print("Too many") 
+    
+    
+        
